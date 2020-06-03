db6d65b7a521c0924899dbaec9561a3d994e9bee
JasonWang <jasonwang@gbd.com>
2020-06-03 14:28:02 +0800

    增加EJS 監控，指令：gulp all:watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,29 @@
 // gulpfile.js
 const gulp = require('gulp');
-// var ejs = require("gulp-ejs");
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 
-// gulp.task('default', defaultTask);
 
-// function defaultTask(done) {
-//     gulp.src("./app/*.ejs")
-//     .pipe(ejs({
-//         msg: "Hello Gulp!"
-//     }))
-//     .pipe(gulp.dest("./dist"))
-// }
-
-// gulp.task('ejs:watch', function () {
-//     gulp.watch("./app/*.ejs", ['defaultTask']); 
-//     // 監控資料夾，當有變化時執行 'sass' 任務
-//   });
+// 編譯 EJS
+gulp.task('ejs', function () {
+  return gulp.src('./app/*.ejs')
+    .pipe(ejs())
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest('./dist'))
+});
 
 gulp.task('sass', function () {     // 定義 sass 的任務名稱
-  return gulp.src('./app/*.scss') // sass 的來源資料夾
+  return gulp.src('./app/scss/*.scss') // sass 的來源資料夾
     .pipe(sass(                     // 編譯 sass
       {outputStyle: 'expanded'}     // sass 的輸出格式
     ).on('error', sass.logError))
-    .pipe(gulp.dest('./dist')); // sass 編譯完成後的匯出資料夾
+    .pipe(gulp.dest('./dist/css')); // sass 編譯完成後的匯出資料夾
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./app/*.scss', gulp.parallel('sass')); 
-  // 監控資料夾，當有變化時執行 'sass' 任務
+// 資料夾監控
+gulp.task('all:watch', function () {
+  gulp.watch('./app/*.ejs', gulp.parallel('ejs'));
+  gulp.watch('./app/scss/*.scss', gulp.parallel('sass')); 
+  // 監控資料夾，當有變化時執行 'ejs/sass' 任務
 });
--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-//載入express模組
-let express = require('express');
+// //載入express模組
+// let express = require('express');
 
 
-// 使用express
-let app = express();
+// // 使用express
+// let app = express();
 
 
-app.get('/', function (req, res) {
-  res.render('index', {
-      'title': '首頁',
-      'titleH2': '<h2>第二級標題</h2>',
-      'show': false,
-      'foods': ['apple', 'banana', 'mongo']
-  });
-});
-app.get('/user', function (req, res) {
-  res.render('user',{
-    'title': '使用者',
-  });
-});
+// app.get('/', function (req, res) {
+//   res.render('index', {
+//       'title': '首頁',
+//       'titleH2': '<h2>第二級標題</h2>',
+//       'show': false,
+//       'foods': ['apple', 'banana', 'mongo']
+//   });
+// });
+// app.get('/user', function (req, res) {
+//   res.render('user',{
+//     'title': '使用者',
+//   });
+// });
 
 
-//設定port位置
-let port = 3000;
-// 監聽 port
-app.listen(port);
+// //設定port位置
+// let port = 3000;
+// // 監聽 port
+// app.listen(port);
 
-let engine = require('ejs-locals');
+// let engine = require('ejs-locals');
 
-app.engine('ejs', engine);
-app.set('views', './views');
-app.set('view engine', 'ejs');
+// app.engine('ejs', engine);
+// app.set('views', './views');
+// app.set('view engine', 'ejs');
